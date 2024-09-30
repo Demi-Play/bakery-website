7ad6ae12f0c2d7cc4040b350504d45169ce0b5ce
D3M1.dev <63244338+Demi-Play@users.noreply.github.com>
2024-09-30 17:15:39 +0300

    db fix mode to write

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import os
 
 db = SQLAlchemy()
 migrate = Migrate()
@@ -10,8 +11,8 @@ login_manager = LoginManager()  # Инициализация LoginManager
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'supedawrseddacreweqtkey'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///bakery.db'
-    # app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/bakery.db'
+    # Используем переменную окружения для определения режима базы данных
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///bakery.db')
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
